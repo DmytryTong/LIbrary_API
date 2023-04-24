dd308aaf2e8a3fb5efc98f632bbf0c08a16489fe
Dmytry <donchykd@gmail.com>
2023-04-24 14:25:16 +0300

    Adding borrowing model, serializers for this model, and 2 views for Borrowing list and detail. Also register model in a admin.py

--- borrowing/models.py
+++ borrowing/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Borrowing(models.Model):
+    book = models.ForeignKey(Book, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    borrow_date = models.DateTimeField(auto_now_add=True)
+    expected_return_date = models.DateTimeField()
+    actual_return_date = models.DateTimeField(null=True, blank=True)
+
