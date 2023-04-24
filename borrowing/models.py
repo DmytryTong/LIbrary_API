12f315ec9603dcc1afba5670165a08ac2bb4d489
David Gluschenko <davidglusch@gmail.com>
2023-04-24 18:16:18 +0300

    applied black

--- borrowing/models.py
+++ borrowing/models.py
@@ -7,9 +7,7 @@ from config import settings
 
 class Borrowing(models.Model):
     book = models.ForeignKey(Book, on_delete=models.CASCADE, related_name="borrowing")
-    user = models.ForeignKey(
-        settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowing"
-    )
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name="borrowing")
     borrow_date = models.DateTimeField(auto_now_add=True)
     expected_return_date = models.DateTimeField()
     actual_return_date = models.DateTimeField(null=True, blank=True)
