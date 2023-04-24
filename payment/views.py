89c2002b4ad14e8270c926033f1bb3ea66540332
Max Bilokur <bilmaxim@gmail.com>
2023-04-24 21:55:03 +0300

    payment app added

--- payment/views.py
+++ payment/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
-# Create your views here.
+from payment.models import Payment
+from payment.permissions import IsOwnerOrAdmin
+from payment.serializers import PaymentSerializer
+
+
+class PaymentListView(generics.ListAPIView):
+    queryset = Payment.objects.select_related("borrowing__user")
+    serializer_class = PaymentSerializer
+    permission_classes = [IsAuthenticated, ]
+
+    def get_queryset(self):
+        queryset = self.queryset
+        if not self.request.user.is_staff:
+            return queryset.filter(borrowing__user=self.request.user)
+        return queryset.all()
+
+
+class PaymentDetailView(generics.RetrieveAPIView):
+    queryset = Payment.objects.select_related("borrowing__user").all()
+    serializer_class = PaymentSerializer
+    permission_classes = [IsOwnerOrAdmin, ]
