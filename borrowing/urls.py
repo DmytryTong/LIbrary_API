725982e8fa2cedb6f121f673dbad0f010e27ae32
David Gluschenko <davidglusch@gmail.com>
2023-04-24 17:54:56 +0300

    Added return url

--- borrowing/urls.py
+++ borrowing/urls.py
@@ -1,15 +1,21 @@
 from django.urls import path
 
-from borrowing.views import BorrowingListView, BorrowingDetailView, BorrowingCreateView
+from borrowing.views import (
+    BorrowingListView,
+    BorrowingDetailView,
+    BorrowingCreateView,
+    BorrowingReturnView,
+)
 
 urlpatterns = [
-    path("/", BorrowingListView.as_view(), name="borrowing-list"),
+    path("", BorrowingListView.as_view(), name="borrowing-list"),
     path(
         "<int:id>/",
         BorrowingDetailView.as_view(),
         name="borrowing-detail"
     ),
     path("create/", BorrowingCreateView.as_view(), name="borrowing-create"),
+    path("<int:pk>/return/", BorrowingReturnView.as_view(), name="borrowing-return"),
 ]
 
 app_name = "borrowing"
