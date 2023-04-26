c6a28e13fab8c1135712aaa7f5d88b5cc9028be3
David Gluschenko <davidglusch@gmail.com>
2023-04-26 13:37:37 +0300

    Added swagger documentation

--- config/settings.py
+++ config/settings.py
@@ -151,6 +151,19 @@ REST_FRAMEWORK = {
     ),
 }
 
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Library Service API",
+    "DESCRIPTION": "Manage library borrowings",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+    "SWAGGER_UI_SETTINGS": {
+        "deepLinking": True,
+        "defaultModelRendering": "model",
+        "defaultModelsExpandDepth": 2,
+        "defaultModelExpandDepth": 2,
+    },
+}
+
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(days=2),
     "REFRESH_TOKEN_LIFETIME": timedelta(days=3),
--- config/urls.py
+++ config/urls.py
@@ -18,11 +18,27 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+from drf_spectacular.views import (
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+    SpectacularAPIView
+)
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("users/", include("user.urls", namespace="user")),
-    path("api/book/", include("book.urls", namespace="book")),
+    path("api/books/", include("book.urls", namespace="book")),
     path("api/borrowings/", include("borrowing.urls", namespace="borrowing")),
     path("api/payments/", include("payment.urls", namespace="payments")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/doc/swagger/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/doc/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
